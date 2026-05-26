0c3d6f69952ed123912540c2c64e14235252e348
MACBOOK <macbook@Daiiii-Dz.local>
2026-05-26 13:46:08 +0700

    Complete admin vehicles UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,10 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import DashboardPage from "./pages/DashboardPage";
+import AdminVehiclesPage from "./pages/AdminVehiclesPage";
 import ParkingSlotsPage from "./pages/ParkingSlotsPage";
+import PaymentsPage from "./pages/PaymentsPage";
+import ReservationsPage from "./pages/ReservationsPage";
 import UserDashboardPage from "./pages/UserDashboardPage";
 import UserMyBookingsPage from "./pages/UserMyBookingsPage";
 import UserSettingsPage from "./pages/UserSettingsPage";
@@ -13,6 +16,9 @@ function App() {
         <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/admin-vehicles" element={<AdminVehiclesPage />} />
+        <Route path="/payments" element={<PaymentsPage />} />
+        <Route path="/reservations" element={<ReservationsPage />} />
         <Route path="/user-dashboard" element={<UserDashboardPage />} />
         <Route path="/user-bookings" element={<UserMyBookingsPage />} />
         <Route path="/user-settings" element={<UserSettingsPage />} />
